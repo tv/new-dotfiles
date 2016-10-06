3fe9c188f07ba3014b924a74aeb166cc39aa156b
Tuomas Vähänen <tuomas.vahanen@gmail.com>
2016-10-06 22:26:34 +0300

    Lots of fixes and changes

--- install_casks.sh
+++ install_casks.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 brew cask install\
-    google-chrome-canary \
     dropbox\
     dash\
     virtualbox\
     vlc\
-    firefox\
-    hip-chat\
-    iterm2-beta\
-    spotify\
     the-unarchiver\
-    textmate\
-    harvest
+    textmate
